e8f622152c79c313f56ac884b63d1d6bceac4e3b
Marcus Lima Arrua <45215381+marcuslimaarruda@users.noreply.github.com>
2021-01-28 17:32:20 -0300

    Deixa de ser calculadora e vira a api Person implementando todos os verbos

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public string primeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        public string Endereco { get; set; }
+        public string Sexo { get; set; }
+        
+    }
+
+}
---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,67 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+
+        }
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = clonePerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                id = 1,
+                primeiroNome = "Marcus",
+                UltimoNome = "Arruda",
+                Endereco = "Rua Visconde do Uriguai, 315/1103, Centro/Niteroi/Rj - Brasil",
+                Sexo = "Masculino"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+        private Person clonePerson(int i)
+        {
+            return new Person
+            {
+                id = IncrementAndGet(),
+                primeiroNome = "Pessoa Nome" + i,
+                UltimoNome = "Pessoa Sobrenome" + i,
+                Endereco = "Endereço da pessoa" + i,
+                Sexo = "Feminino"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
