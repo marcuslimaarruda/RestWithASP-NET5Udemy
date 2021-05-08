6939c7c06ffc92f75d9b05e193ff202ecfd9a36f
Marcus Lima Arrua <arruda_arcus@hotmail.com>
2021-05-08 12:44:31 -0300

    Exemplo de serialização utilizando as proriedades do Json

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,12 +1,19 @@
 ﻿
+using System.Text.Json.Serialization;
+
 namespace RestWithASPNETUdemy.Data.VO
 {
+
     public class PersonVO
     {
+        [JsonPropertyName("Codigo")]    
         public long id { get; set; }
+        [JsonPropertyName("Primeiro Nome")]
         public string primeiroNome { get; set; }
+        [JsonPropertyName("Ultimo Nome")]
         public string UltimoNome { get; set; }
         public string Endereco { get; set; }
+        //[JsonIgnore]
         public string Sexo { get; set; }        
     }
 }
